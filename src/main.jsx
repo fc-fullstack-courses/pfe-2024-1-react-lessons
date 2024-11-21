e7ec5878912539689500155689f30303441b2e13
Kyrylo <fresh.js.ftd.@gmail.com>
2024-11-21 18:41:47 +0200

    Add functional component example

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { createRoot } from 'react-dom/client';
 
 // Класовий компонент
 class MyClassComponent extends React.Component {
-  render () {
+  render() {
     // має повертати якусь річ яку реакт буде відмальовувати на його місці
     /*
       реакт малює:
@@ -25,19 +25,37 @@ class MyClassComponent extends React.Component {
         Symbol
       реакт буде кидати помилку якщо змусити його відмалювати звичайний об'єкт
     */
-      const heading = React.createElement('h2', {className: 'articleHeading'}, 'Class Component heading');
+    const heading = React.createElement(
+      'h2',
+      { className: 'articleHeading' },
+      'Class Component heading'
+    );
 
-      const textPar = React.createElement('p', null, 'Lorem Ipsum');
+    const textPar = React.createElement('p', null, 'Lorem Ipsum');
 
-      const article = React.createElement('article', null, heading, textPar );
+    const article = React.createElement('article', null, heading, textPar);
 
-      return article;
+    return article;
   }
 }
 
 // class MyClassComponent extends Component {}
 
+function MyFunctionalComponent() {
+  const heading = React.createElement(
+    'h2',
+    { className: 'articleHeading' },
+    'Function Component heading'
+  );
+
+  const textPar = React.createElement('p', null, '12345');
+
+  const article = React.createElement('article', null, heading, textPar);
+
+  return article;
+}
+
 // Щоб намальовати компонент його закидемо у createElement першим аргументом
-const elem = React.createElement(MyClassComponent);
+const elem = React.createElement(MyFunctionalComponent);
 
 createRoot(document.getElementById('root')).render(elem);
