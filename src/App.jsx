a7a6206b3b29a537fb03df409062960a99d972b0
Kyrylo <fresh.js.ftd.@gmail.com>
2024-11-21 20:45:23 +0200

    Add some conditional rendering examples

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import MyFunctionalComponent from './components/MyFunctionComponent';
 import Greeting from './components/Greeting';
 
 function App() {
+  const shouldClassComponentsRender = Math.random() > 0.5;
+
   const classElem1 = React.createElement(MyClassComponent);
   const classElem2 = React.createElement(MyClassComponent);
 
@@ -11,17 +13,29 @@ function App() {
   const funcElem2 = React.createElement(MyFunctionalComponent);
 
   // return React.createElement('div', null, classElem1, classElem2, funcElem1, funcElem2);
-  return React.createElement(
-    React.Fragment,
-    null,
-    React.createElement(Greeting, { userFirstName: 'John', userLastName: "Doe"}),
-    React.createElement(Greeting, { userFirstName: 'Sarah'}),
-    React.createElement(Greeting, ),
-    classElem1,
-    classElem2,
-    funcElem1,
-    funcElem2
-  );
+  
+  if(shouldClassComponentsRender) {
+    return React.createElement(
+      React.Fragment,
+      null,
+      React.createElement(Greeting, { userFirstName: 'John', userLastName: "Doe"}),
+      React.createElement(Greeting, { userFirstName: 'Sarah'}),
+      React.createElement(Greeting, ),
+      classElem1,
+      classElem2,
+    );
+  } else {
+    return React.createElement(
+      React.Fragment,
+      null,
+      React.createElement(Greeting, { userFirstName: 'John', userLastName: "Doe"}),
+      React.createElement(Greeting, { userFirstName: 'Sarah'}),
+      React.createElement(Greeting, ),
+      funcElem1,
+      funcElem2
+    );
+  }
+ 
 }
 
 export default App;
--- src/components/Greeting/index.jsx
+++ src/components/Greeting/index.jsx
@@ -19,22 +19,69 @@ import './Greeting.css';
 // }
 
 class Greeting extends React.Component {
-
   render() {
     console.log(this.props);
 
     const { userFirstName = '', userLastName = '' } = this.props;
-  
+
     // Ніколи не змінюйте в компоненті пропси які він приймає
     // this.props.userFirstName = "Baddie";
-    
+
+    // Умовний рендерінг - рендерінг певних елементів / компонентів за
+    // певною умовою
+
     const fullName = `${userFirstName} ${userLastName}`.trim();
-  
-    return React.createElement(
-      'p',
-      { className: 'greetingText' },
-      `Hello, ${fullName ? fullName : 'Guest' }.`
-    );
+
+    // if (fullName) {
+    //   return React.createElement(
+    //     'p',
+    //     { className: 'greetingText' },
+    //     `Hello, ${fullName}.`
+    //   );
+    // } else {
+    //   return React.createElement(
+    //     'p',
+    //     { className: 'greetingText' },
+    //     `Hello, 'Guest.`
+    //   );
+    // }
+
+    // if (fullName) {
+    //   return React.createElement(
+    //     'h1',
+    //     { className: 'greetingText' },
+    //     `Hello, ${fullName}.`
+    //   );
+    // } else {
+    //   return React.createElement(
+    //     'p',
+    //     { className: 'guestGreeting' },
+    //     `Hello, Guest.`
+    //   );
+    // }
+
+    let h1;
+    if (fullName) {
+      h1 = React.createElement(
+        'h1',
+        { className: 'greetingText' },
+        `Hello, ${fullName}.`
+      );
+    } else {
+      h1 = React.createElement(
+        'p',
+        { className: 'guestGreeting' },
+        `Hello, Guest.`
+      );
+    }
+
+    return h1;
+
+    // return React.createElement(
+    //   'p',
+    //   { className: 'greetingText' },
+    //   `Hello, ${fullName ? fullName : 'Guest' }.`
+    // );
   }
 }
 
