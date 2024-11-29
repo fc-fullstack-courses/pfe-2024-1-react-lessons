aebc6aa53eec9f7f14bca3a93f8367269f8ecb52
Kyrylo <fresh.js.ftd.@gmail.com>
2024-11-29 18:50:31 +0200

    Add basic loading logic

--- src/components/UsersLoader/index.jsx
+++ src/components/UsersLoader/index.jsx
@@ -4,31 +4,54 @@ class UsersLoader extends Component {
   state = {
     users: [],
     error: null,
-    isLoading: false
+    isLoading: false,
+  };
+
+  componentDidMount() {
+    this.setState({
+      isLoading: true,
+    });
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((users) => {
+        console.log(users);
+
+        this.setState({
+          users,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          error,
+        });
+      }).finally(() => {
+        this.setState({
+          isLoading: false,
+        });
+      });
   }
 
   render() {
     const { users, isLoading, error } = this.state;
 
-    if(isLoading) {
-      return <div>LOADING ...</div>
+    if (isLoading) {
+      return <div>LOADING ...</div>;
     }
 
-    if(error) {
-      return <div>ERROR: {error.message}</div>
+    if (error) {
+      return <div>ERROR: {error.message}</div>;
     }
-    
-    const usersCards = users.map(user => {
-      return <div key={user.id}>
-        <pre>{JSON.stringify(user, null, 4)}</pre>
-      </div>
+
+    const usersCards = users.map((user) => {
+      return (
+        <div key={user.id}>
+          <pre>{JSON.stringify(user, null, 4)}</pre>
+        </div>
+      );
     });
 
-    return (
-      <div>
-        {usersCards}
-      </div>
-    );
+    return <div>{usersCards}</div>;
   }
 }
 
