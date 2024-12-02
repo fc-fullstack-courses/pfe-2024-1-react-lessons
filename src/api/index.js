d921ab9f89cdaa7fd961a4c3b56a852af76b2808
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-02 18:18:08 +0200

    Add custom queryString helper function

--- src/api/index.js
+++ src/api/index.js
@@ -5,13 +5,38 @@
  * @param {string} options.excludeStr
  * @returns {Promise<Array>}
  */
-export function getUsers({ currentPage, results, excludeStr = 'id' }) {
-  // const { currentPage, results, excludeStr = 'id' } = options;
+export function getUsers(options) {
+  const { currentPage, results, excludeStr = 'id', gender = 'female' } = options;
   const usersPromise = fetch(
-    `https://randomuser.me/api/?seed=12345&results=${results}&nat=ua&page=${currentPage}&exc=${excludeStr}`
+    `https://randomuser.me/api/?seed=12345&results=${results}&nat=ua&page=${currentPage}&exc=${excludeStr}&gender=${gender}`
   )
     .then((res) => res.json())
     .then((responseWithUsers) => responseWithUsers.results);
 
   return usersPromise;
 }
+
+/**
+ * 
+ * @param {object} queryObj 
+ * @returns {string}
+ */
+function generateQueryString (queryObj) {
+  const queryEntries = Object.entries(queryObj);
+
+  const queryPairs = queryEntries.map(([key, value]) => `${key}=${value}`);
+  
+  const query = queryPairs.join('&');
+
+  return `?${query}`;
+}
+
+const obj1 = {
+  seed: 12345,
+  results: 10,
+  page: 1,
+}
+
+const str = generateQueryString(obj1);
+
+ console.log(str); // '?seed=12345&results=10&page=1'
