295729577f47337c249f635395eda5c1c62b7277
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-02 19:44:47 +0200

    Add MessagesLoader example

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Avatar from './components/Avatar';
 import GuestAvatar from './components/GuestAvatar';
 import ComponentA from './components/drillingComponents/A';
 import UsersLoader from './components/UsersLoader';
+import MessagesLoader from './components/MessagesLoader';
 
 // звичайний елемент у реакті
 const elem1 = React.createElement(
@@ -89,7 +90,8 @@ class App extends React.Component {
 
     return (
       <>
-        <UsersLoader />
+        <MessagesLoader />
+        {/* <UsersLoader /> */}
         {/* <Header
           user={user}
           handleLogout={this.handleLogout}
---
+++ src/components/MessagesLoader/index.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class MessagesLoader extends Component {
+  state = {
+    messages: [],
+    isLoading: false,
+    error: null,
+  };
+
+  componentDidMount() {
+    this.setState({
+      isLoading: true,
+    });
+
+    fetch('/messages.json')
+      .then((res) => res.json())
+      .then((messages) => {
+        console.log(messages);
+        this.setState({
+          messages,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          error,
+        });
+      })
+      .finally(() => {
+        this.setState({
+          isLoading: false,
+        });
+      });
+  }
+
+  render() {
+    const { messages, isLoading, error } = this.state;
+
+    const messagesElems = messages.map((message) => (
+      <article key={message.id}>
+        <h2>{message.title}</h2>
+        <h3>By {message.author}</h3>
+        <p>{message.text}</p>
+      </article>
+    ));
+
+    return (
+      <div>
+        {isLoading && <div>LOADING ...</div>}
+        {error && <div>ERROR: {error.message}</div>}
+        {messagesElems}
+      </div>
+    );
+  }
+}
+
+export default MessagesLoader;
