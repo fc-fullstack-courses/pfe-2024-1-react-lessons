593bd601f70da0866d2c59e3e7f404d819abf98c
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-03 20:40:54 +0200

    Make ComponentC worse

--- src/components/drillingComponents/C/index.jsx
+++ src/components/drillingComponents/C/index.jsx
@@ -4,42 +4,41 @@ import { UserContext, ThemeContext } from '../../../contexts';
 import styles from './C.module.css';
 import CONSTANTS from '../../../configs';
 
-
 const ComponentC = (props) => {
   const { forC, ...restOfCProps } = props;
 
-  const renderWithThemeContext = ([theme]) => {
-    let currentThemeClass;
-
-    if (theme === CONSTANTS.THEMES.DARK_THEME) {
-      currentThemeClass = styles.darkTheme;
-    } else if (theme === CONSTANTS.THEMES.LIGHT_THEME) {
-      currentThemeClass = styles.lightTheme;
-    }
-
-    const className = `${styles.container} ${currentThemeClass}`;
-
-    return (
-      <UserContext.Consumer>
-        {(user) => {
-          return (
-            <article className={className}>
-              <h2>ComponentC</h2>
-              <p>{forC}</p>
-              <p>
-                {user.firstName} {user.lastName}
-              </p>
-              <p>Поточна тема: {theme}</p>
-              <ComponentD {...restOfCProps} />
-            </article>
-          );
-        }}
-      </UserContext.Consumer>
-    );
-  };
-
   return (
-    <ThemeContext.Consumer>{renderWithThemeContext}</ThemeContext.Consumer>
+    <ThemeContext.Consumer>
+      {([theme]) => {
+        let currentThemeClass;
+
+        if (theme === CONSTANTS.THEMES.DARK_THEME) {
+          currentThemeClass = styles.darkTheme;
+        } else if (theme === CONSTANTS.THEMES.LIGHT_THEME) {
+          currentThemeClass = styles.lightTheme;
+        }
+
+        const className = `${styles.container} ${currentThemeClass}`;
+
+        return (
+          <UserContext.Consumer>
+            {(user) => {
+              return (
+                <article className={className}>
+                  <h2>ComponentC</h2>
+                  <p>{forC}</p>
+                  <p>
+                    {user.firstName} {user.lastName}
+                  </p>
+                  <p>Поточна тема: {theme}</p>
+                  <ComponentD {...restOfCProps} />
+                </article>
+              );
+            }}
+          </UserContext.Consumer>
+        );
+      }}
+    </ThemeContext.Consumer>
   );
 };
 
