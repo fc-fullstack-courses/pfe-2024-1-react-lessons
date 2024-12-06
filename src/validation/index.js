007460f9165f69bda8f89433db77b0e48d3d8ba3
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-06 19:54:49 +0200

    Move schemas to validation folder

---
+++ src/validation/index.js
@@ -0,0 +1,9 @@
+import * as yup from 'yup';
+
+export const REGISTRATION_SCHEMA = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%_-]).{8,32}$/, 'Password must be normal').required(),
+  nickname: yup.string().matches(/^[A-Za-z]\w{7,63}$/, 'Nickname must start with letter and be 8 - 64 symbols').required(),
+  isSubscribed: yup.boolean().required(),
+  gender: yup.string(),
+});
