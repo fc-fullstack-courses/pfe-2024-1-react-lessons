9098dc97cdb9b9118217b3e2d0af9a7d0504a9d2
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-06 20:55:32 +0200

    Refactor RegistrationForm with Form and Field components

--- src/components/RegistrationForm/index.jsx
+++ src/components/RegistrationForm/index.jsx
@@ -23,20 +23,32 @@ function RegistrationForm(props) {
 
   return (
     <Formik initialValues={initialState} onSubmit={handleSubmit}>
-      {(formikProps) => {
-        // console.log(formikProps);
-        return (
-          <form onSubmit={formikProps.handleSubmit}>
-            <input
-              type='email'
-              name='email'
-              value={formikProps.values.email}
-              onChange={formikProps.handleChange}
-            />
-            <button type='submit'>Відправити форму</button>
-          </form>
-        );
-      }}
+      <Form className={styles.container}>
+        <Field name='email' type='email' placeholder='Enter email' />
+        <Field name='password' type='password' placeholder='Enter password' />
+        <Field name='nickname' placeholder='Enter nickname'/>
+        <Field as='select' name='country'>
+          <option value='UK'>United Kingdom</option>
+          <option value='SWE'>Sweden</option>
+          <option value='UA'>Ukraine</option>
+          <option value='JP'>Japan</option>
+        </Field>
+        <Field as='textarea' name='comment' />
+        <label>
+          <Field type='checkbox' name='isSubscribed'  />
+          <span>Я згоден підписатися на ваш спам</span>
+        </label>
+        <fieldset>
+          <legend>Your gender</legend>
+          <label>
+            <Field type='radio' name='gender' value='male' /> Male
+          </label>
+          <label>
+            <Field type='radio' name='gender' value='female' /> Female
+          </label>
+        </fieldset>
+        <button type='submit'>Register</button>
+      </Form>
     </Formik>
   );
 }
