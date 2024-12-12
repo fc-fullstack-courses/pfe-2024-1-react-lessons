d818fb808391609858f64e3ec0771ca44586d721
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-12 19:39:44 +0200

    Refactor App component to functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,127 +1,51 @@
-import React from 'react';
+import { useState } from 'react';
 import { UserContext, ThemeContext } from './contexts';
 import CONSTANTS from './configs';
-
 import Avatar from './components/Avatar';
-import ProductDashoboard from './components/ProductDashboard';
+import Header from './components/Header';
+import MouseTracker from './components/MouseTracker';
 
-class App extends React.Component {
-  state = {
-    user: {
+function App() {
+  const [user, setUser] = useState({
+    id: 0,
+    firstName: 'Petro',
+    lastName: 'Userenko',
+    isMale: true,
+    email: 'testuser@gmail.com',
+    age: 12,
+  });
+  const [theme, setTheme] = useState(CONSTANTS.THEMES.LIGHT_THEME);
+  const [isTrackerShown, setIsTrackerShown] = useState(false);
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
+  const handleLogin = () => {
+    setUser({
       id: 0,
-      firstName: 'Petro',
+      firstName: 'User',
       lastName: 'Userenko',
       isMale: true,
       email: 'testuser@gmail.com',
       age: 12,
-    },
-    theme: CONSTANTS.THEMES.LIGHT_THEME,
-    isTrackerShown: false,
-    counter: 0
-  };
-
-  componentDidMount() {
-    // console.log('componentDidMount');
-
-    // this.intervalId = setInterval(() => {
-    //   console.log('interval');
-    //   console.log(this.state.counter);
-    // }, 1000);
-  }
-  
-  componentWillUnmount() {
-    // console.log('componentWillUnmount');
-    // clearInterval(this.intervalId);
-  }
-  
-
-
-  handleLogout = () => {
-    this.setState({
-      user: null,
     });
   };
 
-  handleLogin = () => {
-    this.setState({
-      user: {
-        id: 0,
-        firstName: 'User',
-        lastName: 'Userenko',
-        isMale: true,
-        email: 'testuser@gmail.com',
-        age: 12,
-      },
-    });
+  const toggleTracker = () => {
+    setIsTrackerShown(!isTrackerShown);
   };
 
-  switchTheme = (newTheme) => {
-    this.setState({
-      theme: newTheme,
-    });
-  };
-
-  toggleTracker = () => {
-    this.setState({
-      isTrackerShown : !this.state.isTrackerShown
-    })
-  }
-
-  handleClick = () => {
-    // this.setState({
-    //   counter: this.state.counter + 2
-    // });
-
-    // console.log(this.state.counter); // 0
-
-    // this.setState({
-    //   counter: this.state.counter + 1 // 0 + 1 
-    // });
-
-    // коллбек режим setState
-    this.setState((state, props) => {
-      // state та props у колбеці гарантовано з найсвіжішими даними
-
-      console.log(props);
-
-      console.log(state.counter); // 0
-
-      // повертаємо з колбеку об'єкт стану який треба докинути
-      return {
-        counter: state.counter + 1 // 0 + 1
-      }
-    });
-
-    // console.log(this.state.counter); // 0
-
-    // this.setState({
-    //   counter: this.state.counter + 1 // 0 + 1 
-    // });
-
-    this.setState((state) => {
-      console.log(this.state.counter); 
-      console.log(state.counter); // 1
-
-      return {
-        counter: state.counter + 1 // 1 + 1
-      }
-    });
-
-
-    // console.log(this.state.counter); // 0
-  }
-
-  render() {
-    const { user, theme } = this.state;
-    return (
-      <UserContext.Provider value={user}>
-        <ThemeContext.Provider value={[theme, this.switchTheme]}>
-          <Avatar src='test' alt='test'/>
-          <ProductDashoboard />
-        </ThemeContext.Provider>
-      </UserContext.Provider>
-    );
-  }
+  return (
+    <UserContext.Provider value={user}>
+      <ThemeContext.Provider value={[theme, setTheme]}>
+        <Header handleLogin={handleLogin} handleLogout={handleLogout} />
+        <Avatar src='test' alt='test' />
+        <button onClick={toggleTracker}>toggle tracker</button>
+        {isTrackerShown && <MouseTracker />}
+      </ThemeContext.Provider>
+    </UserContext.Provider>
+  );
 }
 
 export default App;
