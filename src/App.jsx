e9c16db56b33c7e87ada5f538ee4e15438168f23
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-12 20:44:50 +0200

    Add basic useReducer example

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,182 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import { UserContext, ThemeContext } from './contexts';
 import CONSTANTS from './configs';
 import Avatar from './components/Avatar';
 import Header from './components/Header';
 import MouseTracker from './components/MouseTracker';
 
-function App() {
-  const [user, setUser] = useState({
+/*
+    редюсер приймає поточне значення стану та action який йому надіслав dispatch
+    та має на їх основі повернути нове значення стану. Запускається реактом коли хтось запускає
+    dispatch
+ */
+function reducer(state, action) {
+  console.log(state);
+  console.log(action);
+
+  switch (action.type) {
+    case 'changeEmail': {
+      const newState = {
+        ...state,
+        email: action.email,
+      };
+
+      return newState;
+    }
+    case 'changeComment': {
+      const newState = {
+        ...state,
+        comment: action.comment,
+      };
+
+      return newState;
+    }
+    case 'changeTheme': {
+      const newState = {
+        ...state,
+        theme: action.theme,
+      };
+
+      return newState;
+    }
+    case 'loginUser': {
+      const newState = {
+        ...state,
+        user: action.user,
+      };
+
+      return newState;
+    }
+    case 'logoutUser': {
+      const newState = {
+        ...state,
+        user: null,
+      };
+
+      return newState;
+    }
+    case 'toggleTracker' : {
+      return {
+        ...state,
+        isTrackerShown: !state.isTrackerShown
+      }
+    }
+  }
+
+  return state;
+}
+
+const initialState = {
+  email: '',
+  comment: '',
+  user: {
     id: 0,
     firstName: 'Petro',
     lastName: 'Userenko',
     isMale: true,
     email: 'testuser@gmail.com',
     age: 12,
-  });
-  const [theme, setTheme] = useState(CONSTANTS.THEMES.LIGHT_THEME);
-  const [isTrackerShown, setIsTrackerShown] = useState(false);
-  const [email, setEmail] = useState('test');
-  const [comment, setComment] = useState('');
+  },
+  theme: CONSTANTS.THEMES.LIGHT_THEME,
+  isTrackerShown: false
+};
+
+function App() {
+  /*
+    useReducer - хук для роботи зі станом замість useState
+      приймає:
+        1. reducer - функція яка має повернути нове значення стану
+        2. initialState - початкове значення стану
+      повертає кортеж з 2 значень
+        1. поточне значання стану
+        2. функція dispatch яка дозвоить змінити стан
+  */
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // const [isTrackerShown, setIsTrackerShown] = useState(false);
 
   const handleLogout = () => {
-    setUser(null);
+    dispatch({ type: 'logoutUser' });
   };
 
   const handleLogin = () => {
-    setUser({
-      id: 0,
-      firstName: 'User',
-      lastName: 'Userenko',
-      isMale: true,
-      email: 'testuser@gmail.com',
-      age: 12,
+    dispatch({
+      type: 'loginUser',
+      user: {
+        id: 0,
+        firstName: 'User',
+        lastName: 'Userenko',
+        isMale: true,
+        email: 'testuser@gmail.com',
+        age: 12,
+      },
+    });
+  };
+
+  const changeTheme = (newTheme) => {
+    dispatch({
+      type: 'changeTheme',
+      theme: newTheme,
     });
   };
 
   const toggleTracker = () => {
-    setIsTrackerShown(!isTrackerShown);
+    dispatch({ type: 'toggleTracker' });
   };
 
-  const changeEmail = ({target: {value}}) => {
-    setEmail(value);
-  }
+  const changeEmail = ({ target: { value } }) => {
+    /*
+      Принципп роботи dispatch
+        При виклику dispatch ви йому маєте передати один аргумент (action)
+        у цьому аргументі має бути достатньо деталей щоб зрозуміти як треба змінити стан
 
-  const changeComment = ({target: {value}}) => {
-    setComment(value);
-  }
+        після запуску dispatch реакт запускає функцію reducer
+    */
 
-  const submitForm =  (e) => {
+    const action = {
+      type: 'changeEmail',
+      email: value,
+    };
+
+    dispatch(action);
+  };
+
+  const changeComment = ({ target: { value } }) => {
+    const action = {
+      type: 'changeComment',
+      comment: value,
+    };
+
+    dispatch(action);
+  };
+
+  const submitForm = (e) => {
     e.preventDefault();
 
     alert(`ми з вами зв'яжемося.`);
-  }
+  };
 
   return (
-    <UserContext.Provider value={user}>
-      <ThemeContext.Provider value={[theme, setTheme]}>
+    <UserContext.Provider value={state.user}>
+      <ThemeContext.Provider value={[state.theme, changeTheme]}>
         <Header handleLogin={handleLogin} handleLogout={handleLogout} />
         <Avatar src='test' alt='test' />
+        <button onClick={toggleTracker}>toggle tracker</button>
+        {state.isTrackerShown && <MouseTracker />}
         <form onSubmit={submitForm}>
-          <input type='email' name='email' value={email} onChange={changeEmail} />
-          <textarea name='comment' value={comment} onChange={changeComment} />
+          <input
+            type='email'
+            name='email'
+            value={state.email}
+            onChange={changeEmail}
+          />
+          <textarea
+            name='comment'
+            value={state.comment}
+            onChange={changeComment}
+          />
           <button>Send comment</button>
         </form>
-        <button onClick={toggleTracker}>toggle tracker</button>
-        {isTrackerShown && <MouseTracker />}
       </ThemeContext.Provider>
     </UserContext.Provider>
   );
