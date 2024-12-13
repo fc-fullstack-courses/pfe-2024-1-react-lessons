c13be83b29bc37e514820a0dcc3f44954bf73be5
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-13 21:08:04 +0200

    Fix Header by removing context

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,5 @@
-import { useContext, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { Link } from 'react-router';
-import cx from 'classnames';
-import { ThemeContext, UserContext } from '../../contexts';
-import styles from './Header.module.scss';
-import CONSTANTS from '../../configs';
 
 function Header({ dispatch }) {
   const inputRef = useRef(null);
@@ -26,76 +22,23 @@ function Header({ dispatch }) {
 
   console.log(rendersRef.current);
 
-  // useContext приймає об'єкт контексту та повертає значення яке у ньому знаходиться
-  const [theme, switchTheme] = useContext(ThemeContext);
-  const user = useContext(UserContext);
-
-  // const { x, y } = useMouseTracker(containerRef);
-
-  const handleLogout = () => {
-    dispatch({ type: 'logoutUser' });
-  };
-
-  const handleLogin = () => {
-    dispatch({
-      type: 'loginUser',
-      user: {
-        id: 0,
-        firstName: 'User',
-        lastName: 'Userenko',
-        isMale: true,
-        email: 'testuser@gmail.com',
-        age: 12,
-      },
-    });
-  };
-
-  const fullName = user ? `${user.firstName} ${user.lastName}`.trim() : 'Guest';
-
-  const logoutBtn = <button onClick={handleLogout}>Вийти</button>;
-  const loginBtn = <button onClick={handleLogin}>Увійти</button>;
-
-  const headerClasses = cx(styles.container, {
-    [styles.darkTheme]: theme === CONSTANTS.THEMES.DARK_THEME,
-    [styles.lightTheme]: theme === CONSTANTS.THEMES.LIGHT_THEME,
-    [styles.blackTheme]: theme === CONSTANTS.THEMES.BLACK_THEME,
-  });
-
   return (
-    <header className={headerClasses} ref={containerRef}>
+    <header ref={containerRef}>
       <h1>Мій сайт</h1>
-      <p>Привіт {fullName}</p>
-      {user ? logoutBtn : loginBtn}
-      <button
-        onClick={() => {
-          const newTheme =
-            theme === CONSTANTS.THEMES.DARK_THEME
-              ? CONSTANTS.THEMES.LIGHT_THEME
-              : CONSTANTS.THEMES.DARK_THEME;
-
-          dispatch({
-            type: 'changeTheme',
-            theme: newTheme,
-          });
-        }}
-      >
-        Змінити тему на {theme === 'dark' ? 'світлу' : 'темну'}
-      </button>
       {/* прив'язка рефа до елемента */}
       <input ref={inputRef} />
-      {/* <p>X: {x}</p>
-      <p>Y: {y}</p> */}
-          <ul>
-      <li>
-        <Link to='/'>Home page</Link>
-      </li>
-      <li>
-        <Link to='/about'>About page</Link>
-      </li>
-      <li>
-        <Link to='/contacts'>Contacts page</Link>
-      </li>
-    </ul>
+
+      <ul>
+        <li>
+          <Link to='/'>Home page</Link>
+        </li>
+        <li>
+          <Link to='/about'>About page</Link>
+        </li>
+        <li>
+          <Link to='/contacts'>Contacts page</Link>
+        </li>
+      </ul>
     </header>
   );
 }
