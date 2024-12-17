3c45665db35f07af0707083bae9d7f1d38f25828
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-17 20:33:27 +0200

    Add very basic dropdown list example

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,26 @@ export function HomePage() {
     <div className={styles.container}>
       {/* <Header /> */}
       Home Page.
+      <ul className={styles.navList}>
+        <li className={styles.navListItem}>
+          <span>Заголовок 1</span>
+          <ul className={styles.subNavList}>
+            <li className={styles.subNavListItem}>Посилання 1</li>
+            <li className={styles.subNavListItem}>Посилання 1</li>
+            <li className={styles.subNavListItem}>Посилання 1</li>
+            <li className={styles.subNavListItem}>Посилання 1</li>
+          </ul>
+        </li>
+        <li className={styles.navListItem}>
+          <span>Заголовок 2</span>
+          <ul className={styles.subNavList}>
+            <li className={styles.subNavListItem}>Посилання 1</li>
+            <li className={styles.subNavListItem}>Посилання 1</li>
+            <li className={styles.subNavListItem}>Посилання 1</li>
+            <li className={styles.subNavListItem}>Посилання 1</li>
+          </ul>
+        </li>
+      </ul>
       {/* <Footer /> */}
     </div>
   );
